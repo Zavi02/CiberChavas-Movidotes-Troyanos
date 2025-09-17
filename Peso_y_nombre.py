ac4334aac4db010f5f404a70625e1beb9cbe013b
Chavas <117127377+Zavi02@users.noreply.github.com>
2025-09-17 09:17:46 +0200

    Add files via upload

---
+++ Peso_y_nombre.py
@@ -0,0 +1,10 @@
+# Script: nombre_y_peso.py
+
+# Pedir datos al usuario
+nombre = input("Como te llamas?: ")
+peso = input("Cuanto pesas?: ")
+
+# Mostrar los datos ingresados
+
+print(f"Te llamas: {nombre}")
+print(f"y tu peso es: {peso} kg (aproximadamente)")
